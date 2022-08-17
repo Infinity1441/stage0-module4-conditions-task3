cd6163f78f9465b25daf6e744098c3219b50afcf
Infinity1441 <110477091+Infinity1441@users.noreply.github.com>
2022-08-17 12:38:33 +0500

    Update AlphabeticCharacters.java

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (!Character.isAlphabetic(character))
+            System.out.println("wrong alphabet!");
+        else {
+            if (character == 'e' || character == 'u' || character == 'i' || character == 'o' || character == 'a')
+                System.out.println("Vowel");
+            else
+                System.out.println("Consonant");
+        }
     }
 }
