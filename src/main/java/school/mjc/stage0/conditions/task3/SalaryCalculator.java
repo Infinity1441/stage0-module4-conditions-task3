74ab474b8ceea7422f002fe045f7f6458733d019
Infinity1441 <110477091+Infinity1441@users.noreply.github.com>
2022-08-17 19:16:45 +0500

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,16 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        if(salary < 0)
+        if (salary < 0)
             System.out.println("wrong input!");
-        else
-        if(salary <= 10000) 
-            System.out.println(salary*0.85);
-        else
-            if(10000 < salary <= 20000)
-                System.out.println(salary*0.82);
-        else
-            if(salary > 20000)
-                System.out.println(salary*0.8);
+        else if (salary <= 10000)
+            System.out.println(salary * 0.85);
+        else if (10000 < salary && salary <= 20000)
+            System.out.println(salary * 0.82);
+        else if (salary > 20000)
+            System.out.println(salary * 0.8);
     }
 }
